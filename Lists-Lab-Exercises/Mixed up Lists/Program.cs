542b80255972ec1a4cea5a037d9b391cb6ff955a
Dimitar Betsov <d.betsov@gmail.com>
2019-02-22 11:17:56 +0200

    Did more exercises

---
+++ Lists-Lab-Exercises/Mixed up Lists/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mixed_up_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> range = new List<int>();
+            List<int> mixedList = new List<int>();
+            List<int> sortedResult = new List<int>();
+
+            for (int i = 0; i < Math.Min(firstList.Count,secondList.Count); i++)
+            {
+                mixedList.Add(firstList[i]);
+                mixedList.Add(secondList[secondList.Count-1-i]);
+            }
+            if (firstList.Count > secondList.Count)
+            {
+                range.Add(firstList[firstList.Count - 1]);
+                range.Add(firstList[firstList.Count - 2]);
+            }
+            else
+            {
+                range.Add(secondList[0]);
+                range.Add(secondList[1]);
+            }
+
+            for (int i = 0; i < mixedList.Count; i++)
+            {
+                if (mixedList[i] > Math.Min(range[0],range[1]) && mixedList[i] < Math.Max(range[0], range[1]))
+                {
+                    sortedResult.Add(mixedList[i]);
+                }
+            }
+            sortedResult.Sort();
+            Console.WriteLine(string.Join(" ", sortedResult));
+        }
+    }
+}
---
+++ Lists-Lab-Exercises/Take-Skip Rope/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Take_Skip_Rope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<char> characters = new List<char>();
+            List<double> numbers = new List<double>();
+            List<int> take = new List<int>();
+            List<int> skip = new List<int>();
+
+            
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if ((int)input[i] > 47 && (int)input[i] < 58)
+                {
+                    numbers.Add(char.GetNumericValue(input[i]));
+                }
+                else
+                {
+                    characters.Add(input[i]);
+                }
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (i % 2 ==0)
+                {
+                    take.Add((int)numbers[i]);
+                }
+                else
+                {
+                    skip.Add((int)numbers[i]);
+                }
+            }
+            int commandLength = take.Count;
+            int currPossition = 0;
+            string ressult = "";
+
+            for (int i = 0; i < commandLength; i++)
+            {
+                for (int j = currPossition; j < Math.Min((currPossition + take[i]),characters.Count); j++)
+                {
+                    ressult += characters[j];
+                }
+                currPossition += take[i];
+                currPossition += skip[i];
+            }
+            Console.WriteLine(ressult);
+
+        }
+    }
+}
