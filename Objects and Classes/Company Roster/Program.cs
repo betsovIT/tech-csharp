650c7ad76c5312e8aacb4b04ae7b14baa7834e97
Dimitar Betsov <d.betsov@gmail.com>
2019-02-27 21:46:54 +0200

    Solved some more problems

---
+++ Objects and Classes/Company Roster/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Company_Roster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toRead = int.Parse(Console.ReadLine());
+            List<Department> departments = new List<Department>();
+
+            for (int i = 0; i < toRead; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                bool foundDepartment = false;
+
+                Employee employee = new Employee();
+                employee.Name = input[0];
+                employee.Salary = double.Parse(input[1]);
+                employee.Department = input[2];
+
+                for (int j = 0; j < departments.Count; j++)
+                {
+                    if (employee.Department == departments[j].Name)
+                    {
+                        departments[j].Employees.Add(employee);
+                        foundDepartment = true;
+                        break;
+                    }
+                }
+
+                if (!foundDepartment)
+                {
+                    Department department = new Department();
+                    department.Name = input[2];
+                    department.Employees.Add(employee);
+                    departments.Add(department);
+                }
+            }
+            double maxAvSalary = 0;
+            string richestDepartment = "";
+
+            for (int i = 0; i < departments.Count; i++)
+            {
+                double sumSalary = 0;
+                for (int k = 0; k < departments[i].Employees.Count; k++)
+                {
+                    sumSalary += departments[i].Employees[k].Salary;
+                }
+
+                if (sumSalary > maxAvSalary)
+                {
+                    maxAvSalary = sumSalary;
+                    richestDepartment = departments[i].Name;
+                }
+            }
+
+            for (int i = 0; i < departments.Count; i++)
+            {
+                if (departments[i].Name == richestDepartment)
+                {
+                    Console.WriteLine($"Highest Average Salary: {departments[i].Name}");
+                    departments[i].Employees.Sort((x, y) => x.Salary.CompareTo(y.Salary));
+                    departments[i].Employees.Reverse();
+
+                    for (int j = 0; j < departments[i].Employees.Count; j++)
+                    {
+                        Console.WriteLine($"{departments[i].Employees[j].Name} {departments[i].Employees[j].Salary:F2}");
+                    }
+                }
+            }
+        }
+    }
+    class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string Department { get; set; }
+    }
+    class Department
+    {
+        public Department()
+        {
+            Employees = new List<Employee>();
+        }
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; }
+        
+    }
+}
---
+++ Objects and Classes/Oldest Family Member/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Oldest_Family_Member
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class Family
+    {
+        public Family()
+        {
+            people = new List<Person>();
+        }
+
+        public List<Person> people { get; set; }
+
+        public void AddMember(Person member)
+        {
+            people.Add(member);
+        }
+        public Person GetOldestMember()
+        {
+            if (people.Count>0)
+            {
+                int age = int.MinValue;
+                int personNumber = -1;
+                for (int i = 0; i < people.Count; i++)
+                {
+                    if (people[i].Age > age)
+                    {
+                        age = people[i].Age;
+                        personNumber = i;
+                    }
+                }
+                return people[personNumber];
+            }
+            else
+            {
+                return null;
+            }
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+            int members = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < members; i++)
+            {
+                Person person = new Person();
+
+                string[] input = Console.ReadLine().Split();
+
+                person.Name = input[0];
+                person.Age = int.Parse(input[1]);
+
+                family.AddMember(person);
+            }
+
+            Console.WriteLine($"{family.GetOldestMember().Name} {family.GetOldestMember().Age}");
+        }
+    }
+
+}
