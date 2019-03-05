2473a822681c323bf064d53656d1e00274a438a1
Dimitar Betsov <d.betsov@gmail.com>
2019-03-05 20:01:48 +0200

    Задачки...

---
+++ Prep exams/Prep exams/Grains of Sand/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Grains_of_Sand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sandWatches = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "Mort")
+            {
+                string[] command = input.Split();
+                if (command[0] == "Add")
+                {
+                    sandWatches.Add(int.Parse(command[1]));
+                }
+                else if (command[0] == "Remove")
+                {
+                    if (sandWatches.Contains(int.Parse(command[1])))
+                    {
+                        sandWatches.Remove(int.Parse(command[1]));
+                    }
+                    else
+                    {
+                        if (int.Parse(command[1]) < sandWatches.Count)
+                        {
+                            sandWatches.RemoveAt(int.Parse(command[1]));
+                        }
+                    }
+                }
+                else if (command[0] == "Replace")
+                {
+                    if (sandWatches.Contains(int.Parse(command[1])))
+                    {
+                        int tempIndex = sandWatches.FindIndex(n => n == int.Parse(command[1]));
+                        sandWatches[tempIndex] = int.Parse(command[2]);
+                    }
+                }
+                else if (command[0] == "Increase")
+                {
+                    int value = int.Parse(command[1]);
+                    if (sandWatches.Exists(n => n >= value))
+                    {
+                        int index = sandWatches.FindIndex(n => n >= value);
+                        int valueToIncrease = sandWatches[index];
+
+                        for (int i = 0; i < sandWatches.Count; i++)
+                        {
+                            sandWatches[i] += valueToIncrease;
+                        }
+                    }
+                    else
+                    {
+                        int valueToIncrease = sandWatches[sandWatches.Count - 1];
+                        for (int i = 0; i < sandWatches.Count; i++)
+                        {
+                            sandWatches[i] += valueToIncrease;
+                        }
+                    }                    
+                }
+                else if (command[0] == "Collapse")
+                {
+                    for (int i = 0; i < sandWatches.Count; i++)
+                    {
+                        if (sandWatches[i] < int.Parse(command[1]))
+                        {
+                            sandWatches.RemoveAt(i);
+                            i--;
+                        }
+                    }
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ",sandWatches));
+        }
+    }
+}
---
+++ Prep exams/Prep exams/Hogswatch/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Hogswatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int homesToVisit = int.Parse(Console.ReadLine());
+            int initPresents = int.Parse(Console.ReadLine());
+            int currentPresents = initPresents;
+
+            int[] homes = new int[homesToVisit];
+            int returnCounter = 0;
+            int totalAditionalPresents = 0;
+
+            int visitedHomes = 0;
+            int additionalPresents = 0;
+
+            for (int i = 0; i < homesToVisit; i++)
+            {
+                homes[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < homes.Length; i++)
+            {
+                if (currentPresents >= homes[i])
+                {
+                    currentPresents -= homes[i];
+                    visitedHomes++;
+                    continue;
+                }
+                if (currentPresents < homes[i])
+                {
+                    returnCounter++;
+                    additionalPresents = homes[i] - currentPresents;
+                    homes[i] -= currentPresents;
+                    visitedHomes++;
+                    currentPresents = initPresents / visitedHomes * (homes.Length - visitedHomes) + additionalPresents;
+                    totalAditionalPresents += currentPresents;
+                    visitedHomes--;
+                    i--;
+
+                }
+            }
+            if (returnCounter == 0)
+            {
+                Console.WriteLine(currentPresents);
+            }
+            else
+            {
+                Console.WriteLine(returnCounter);
+                Console.WriteLine(totalAditionalPresents);
+            }
+        }
+    }
+}
