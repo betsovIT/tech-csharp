66b4776f4fc2fea0d8d58c04ad0ea0736f678167
Dimitar Betsov <d.betsov@gmail.com>
2019-03-22 11:32:00 +0200

    Solved the last two problems

---
+++ Text-Processing-and-Regular-Expressions/Letters Change Numbers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Letters_Change_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Много грозна хватка със разделяне чрез табулация - в условието пише един или повече интервала...
+            string[] input = Console.ReadLine().Split(new[] { " ", "\t" }, StringSplitOptions.RemoveEmptyEntries);
+
+            Regex regex = new Regex(@"^(?<firstLetter>[A-Za-z]{1})(?<num>\d+)(?<secondLetter>[A-Za-z]{1})$");
+
+            decimal sum = 0M;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char firstLetter = char.Parse(regex.Match(input[i]).Groups["firstLetter"].Value);
+                decimal num = decimal.Parse(regex.Match(input[i]).Groups["num"].Value);
+                char secondLetter = char.Parse(regex.Match(input[i]).Groups["secondLetter"].Value);
+
+                if (char.IsUpper(firstLetter))
+                {
+                    int position = (int)firstLetter - 64;
+                    num = num / position;
+
+                }
+                else if (char.IsLower(firstLetter))
+                {
+                    int position = (int)firstLetter - 96;
+                    num = num * position;
+                }
+
+                if (char.IsUpper(secondLetter))
+                {
+                    int position = (int)secondLetter - 64;
+                    num = num - position;
+
+                }
+                else if (char.IsLower(secondLetter))
+                {
+                    int position = (int)secondLetter - 96;
+                    num = num + position;
+                }
+                sum += num;
+            }
+
+            Console.WriteLine($"{sum:F2}");
+
+        }
+    }
+}
---
+++ Text-Processing-and-Regular-Expressions/SoftUni Bar Income/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"(?<name>%[A-Z]{1}[a-z]+%)()[^|$%.]*(?<product><\w+>)[^|$%.]*(?<count>\|\d+\|)[^|$%.]*?(?<price>\d+[\.\d]*\$)");
+            double totalIncome = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end of shift")
+                {
+                    break;
+                }
+
+                if (regex.IsMatch(input))
+                {
+                    Match match = regex.Match(input);
+
+                    string customerNameRaw = match.Groups["name"].Value;
+                    string customerName = customerNameRaw.Substring(1, customerNameRaw.Length - 2);
+                    string productRaw = match.Groups["product"].Value;
+                    string product = productRaw.Substring(1, productRaw.Length - 2);
+                    string countRaw = match.Groups["count"].Value;
+                    int count = int.Parse(countRaw.Substring(1, countRaw.Length - 2));
+                    string priceRaw = match.Groups["price"].Value;
+                    double price = double.Parse(priceRaw.Substring(0,priceRaw.Length-1));
+
+                    double income = count * price;
+                    totalIncome += income;
+
+                    Console.WriteLine($"{customerName}: {product} - {income:F2}");
+
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            Console.WriteLine($"Total income: {totalIncome:F2}");
+        }
+    }
+}
