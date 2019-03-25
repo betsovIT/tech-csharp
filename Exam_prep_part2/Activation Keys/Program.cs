a6615762f3800eb2731380d80c73b5eba3155d9c
Dimitar Betsov <d.betsov@gmail.com>
2019-03-25 10:32:01 +0200

    Solved an exam from 20.12.2018

---
+++ Exam_prep_part2/Activation Keys/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Activation_Keys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rawKeys = Console.ReadLine().Split('&');
+            Regex regex = new Regex(@"^[A-Za-z0-9]+$");
+            var processedKeys = new List<string>();
+
+            for (int i = 0; i < rawKeys.Length; i++)
+            {
+                if (regex.IsMatch(rawKeys[i]) && rawKeys[i].Length == 16)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    string key = rawKeys[i];
+
+                    for (int j = 0; j < 16; j++)
+                    {
+                        if (char.IsLetter(key[j]) && char.IsLower(key[j]))
+                        {
+                            sb.Append(char.ToUpper(key[j]));
+                        }
+                        else if (char.IsDigit(key[j]))
+                        {
+                            int num = int.Parse(key[j].ToString());
+                            sb.Append(9 - num);
+                        }
+                        else if (char.IsUpper(key[j]))
+                        {
+                            sb.Append(key[j]);
+                        }
+
+                        if ((j + 1) % 4 == 0 && j + 1 != key.Length)
+                        {
+                            sb.Append('-');
+                        }
+                    }
+                    processedKeys.Add(sb.ToString());
+                }
+                else if (regex.IsMatch(rawKeys[i]) && rawKeys[i].Length == 25)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    string key = rawKeys[i];
+
+                    for (int j = 0; j < 25; j++)
+                    {
+                        if (char.IsLetter(key[j]) && char.IsLower(key[j]))
+                        {
+                            sb.Append(char.ToUpper(key[j]));
+                        }
+                        else if (char.IsDigit(key[j]))
+                        {
+                            int num = int.Parse(key[j].ToString());
+                            sb.Append(9 - num);
+                        }
+                        else if (char.IsUpper(key[j]))
+                        {
+                            sb.Append(key[j]);
+                        }
+
+                        if ((j + 1) % 5 == 0 && j + 1 != key.Length)
+                        {
+                            sb.Append('-');
+                        }
+                    }
+                    processedKeys.Add(sb.ToString());
+                }
+            }
+            Console.WriteLine(string.Join(", ", processedKeys));
+        }
+    }
+}
---
+++ Exam_prep_part2/Vapor Winter Sale/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vapor_Winter_Sale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] gamesInput = Console.ReadLine().Split(", ");
+            var gameCatalogue = new Dictionary<string, Game>();
+
+            for (int i = 0; i < gamesInput.Length; i++)
+            {
+                string game = gamesInput[i];
+
+                if (game.Contains('-'))
+                {
+                    int specCharIndex = game.IndexOf('-');
+                    string gameName = game.Substring(0, specCharIndex);
+                    double gamePrice = double.Parse(game.Substring(specCharIndex + 1, game.Length - 1 - specCharIndex));
+
+                    Game gameAsObject = new Game { Price = gamePrice };
+
+                    if (!gameCatalogue.ContainsKey(gameName))
+                    {
+                        gameCatalogue.Add(gameName, gameAsObject);
+                    }
+                }
+                else if (game.Contains(':'))
+                {
+                    int specCharIndex = game.IndexOf(':');
+                    string gameName = game.Substring(0, specCharIndex);
+                    string dlcName = game.Substring(specCharIndex + 1, game.Length - 1 - specCharIndex);
+
+                    if (gameCatalogue.ContainsKey(gameName))
+                    {
+                        gameCatalogue[gameName].DLC = dlcName;
+                        gameCatalogue[gameName].Price *= 1.2;
+                    }
+                }
+            }
+
+            foreach (var game in gameCatalogue)
+            {
+                if (game.Value.DLC != null)
+                {
+                    game.Value.Price /= 2;
+                }
+                else
+                {
+                    game.Value.Price *= 0.8;
+                }
+            }
+
+            foreach (var game in gameCatalogue.Where(n => n.Value.DLC != null).OrderBy(n => n.Value.Price))
+            {
+                Console.WriteLine($"{game.Key} - {game.Value.DLC} - {game.Value.Price:F2}");
+            }
+            foreach (var game in gameCatalogue.Where(n => n.Value.DLC == null).OrderByDescending(n => n.Value.Price))
+            {
+                Console.WriteLine($"{game.Key} - {game.Value.Price:F2}");
+            }
+        }
+    }
+    class Game
+    {
+        public double Price { get; set; }
+        public string DLC { get; set; }
+    }
+}
