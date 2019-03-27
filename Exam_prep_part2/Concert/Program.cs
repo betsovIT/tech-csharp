adbdd7a3fb118fc8a74287760b33383da49c4883
Dimitar Betsov <d.betsov@gmail.com>
2019-03-27 21:04:42 +0200

    Solved the exam from 16.12.2018

---
+++ Exam_prep_part2/Concert/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Concert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bandMembers = new Dictionary<string, List<string>>();
+            var bandPlayTime = new Dictionary<string, long>();
+
+            string[] separators = { ", ", "; " };
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                if (input[0] == "start of concert")
+                {
+                    break;
+                }
+
+                if (input[0] == "Add")
+                {
+                    string band = input[1];
+                    var members = new List<string>();
+
+                    for (int i = 2; i < input.Length; i++)
+                    {
+                        members.Add(input[i]);
+                    }
+
+                    if (!bandMembers.ContainsKey(band))
+                    {
+                        bandMembers.Add(band, members);
+                    }
+                    else
+                    {
+                        foreach (var member in members)
+                        {
+                            if (!bandMembers[band].Contains(member))
+                            {
+                                bandMembers[band].Add(member);
+                            }
+                        }
+                    }
+                }
+                else if (input[0] == "Play")
+                {
+                    string band = input[1];
+                    long time = long.Parse(input[2]);
+                    if (!bandPlayTime.ContainsKey(band))
+                    {
+                        bandPlayTime.Add(band, time);
+                    }
+                    else
+                    {
+                        bandPlayTime[band] += time;
+                    }
+
+                }
+            }
+
+            string bandToPrint = Console.ReadLine();
+
+            var orderedByPlayTime = bandPlayTime.OrderByDescending(n => n.Value).ThenBy(n => n.Key);
+
+            Console.WriteLine($"Total time: {bandPlayTime.Sum(n => n.Value)}");
+            foreach (var band in orderedByPlayTime)
+            {
+                Console.WriteLine($"{band.Key} -> {band.Value}");
+            }
+
+            Console.WriteLine(bandToPrint);
+            foreach (var member in bandMembers[bandToPrint])
+            {
+                Console.WriteLine($"=> {member}");
+            }
+        }
+    }
+}
---
+++ Exam_prep_part2/Song Encryption/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Song_Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex artistRegex = new Regex(@"^[A-Z]{1}[a-z' ]*$");
+            Regex songRegex = new Regex(@"^[A-Z ]+$");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+
+                string artist = input.Split(':')[0];
+                string song = input.Split(':')[1];
+
+                if (artistRegex.IsMatch(artist) && songRegex.IsMatch(song))
+                {
+                    int key = artist.Length;
+
+                    StringBuilder sb = new StringBuilder();
+
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        if (input[i] == ':')
+                        {
+                            sb.Append('@');
+                        }
+                        else if (Char.IsLetter(input[i]) && char.IsLower(input[i]))
+                        {
+                            if (input[i] + key <= 122)
+                            {
+                                sb.Append((char)(input[i] + key));
+                            }
+                            else
+                            {
+                                sb.Append((char)(input[i] + key - 26));
+                            }
+                        }
+                        else if (Char.IsLetter(input[i]) && char.IsUpper(input[i]))
+                        {
+                            if (input[i] + key <= 90)
+                            {
+                                sb.Append((char)(input[i] + key));
+                            }
+                            else
+                            {
+                                sb.Append((char)(input[i] + key - 26));
+                            }
+                        }
+                        else
+                        {
+                            sb.Append(input[i]);
+                        }
+                    }
+                    Console.WriteLine($"Successful encryption: {sb.ToString()}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
+        }
+    }
+}
