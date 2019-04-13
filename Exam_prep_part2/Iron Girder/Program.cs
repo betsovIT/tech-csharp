089f157ede29fb5236d0e00e1fba61af9a78c00d
Dimitar Betsov <d.betsov@gmail.com>
2019-04-13 10:33:40 +0300

    Solved some problems from retake exam from 27 August

---
+++ Exam_prep_part2/Iron Girder/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Iron_Girder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var towns = new Dictionary<string, int[]>();
+            string[] separators = { ":", "->" };
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Slide rule")
+                {
+                    break;
+                }
+
+                string town = command.Split(separators, StringSplitOptions.None)[0];
+                string timeOrAmbush = command.Split(separators, StringSplitOptions.None)[1];
+                int passengers = int.Parse(command.Split(separators, StringSplitOptions.None)[2]);
+
+                if (timeOrAmbush != "ambush")
+                {
+                    int time = int.Parse(timeOrAmbush);
+
+                    if (!towns.ContainsKey(town))
+                    {
+                        towns.Add(town, new int[] { 0, 0 });
+                    }
+
+                    if (time < towns[town][0] || towns[town][0] == 0)
+                    {
+                        towns[town][0] = time;
+                    }
+
+                    towns[town][1] += passengers;
+                }
+                else if (timeOrAmbush == "ambush")
+                {
+                    if (!towns.ContainsKey(town))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        towns[town][0] = 0;
+                        towns[town][1] -= passengers;
+                    }
+                }
+            }
+
+            foreach (var town in towns.OrderBy(n => n.Value[0]).ThenBy(n => n.Key))
+            {
+                if (town.Value[0] > 0 && town.Value[1] > 0)
+                {
+                    Console.WriteLine($"{town.Key} -> Time: {town.Value[0]} -> Passengers: {town.Value[1]}");
+                }
+            }
+        }
+    }
+}
---
+++ Exam_prep_part2/Post Office/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Post_Office
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rawInput = Console.ReadLine();
+
+            string capitalLettersRaw = rawInput.Split('|')[0];
+            string wordLengthsRaw = rawInput.Split('|')[1];
+            string wordsToCheckRaw = rawInput.Split('|')[2];
+
+            Regex regexCapitalLetters = new Regex(@"(?<=([#$%&]))[A-Z]+(?=\1)");
+            Regex regexWordLengths = new Regex(@"(?<letter>[\d]{2}):(?<length>[\d]{2})");
+            Regex regexWordsToCheck = new Regex(@"\b[A-Z]{1}[\S]+\b");
+
+            string capitalLetters = regexCapitalLetters.Match(capitalLettersRaw).Value;
+            MatchCollection wordLengths = regexWordLengths.Matches(wordLengthsRaw);
+            MatchCollection wordsToCheck = regexWordsToCheck.Matches(wordsToCheckRaw);
+
+
+            foreach (Match word in wordsToCheck)
+            {
+                bool correctCapLetter = false;
+                bool correctLength = false;
+
+                foreach (var letter in capitalLetters)
+                {
+                    if (letter == word.Value[0])
+                    {
+                        correctCapLetter = true;
+                        break;
+                    }
+                }
+
+                foreach (Match rule in wordLengths)
+                {
+                    if (word.Value[0] == (char)int.Parse(rule.Groups["letter"].Value) && word.Value.Length == int.Parse(rule.Groups["length"].Value) + 1)
+                    {
+                        correctLength = true;
+                    }
+
+                }
+
+                if (correctCapLetter && correctLength)
+                {
+                    Console.WriteLine(word.Value);
+                }
+            }
+        }
+    }
+}
