6116d77dd3dc604c172c6a7541118b18201d364a
Dimitar Betsov <d.betsov@gmail.com>
2019-04-13 13:44:29 +0300

    Solved the problems from 01.07.2018 exam

---
+++ Exam_prep_part2/SoftUni Bar Income/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"(?<name>%[A-Z]{1}[a-z]+%)[^|%$.]*(?<product><[\w]+>)[^|%$.]*(?<quantity>\|\d+\|)[^|%$.]*?(?<price>\d+\.?\d*)\$");
+
+            double totalSum = 0;
+
+            var culture = CultureInfo.InvariantCulture;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end of shift")
+                {
+                    break;
+                }
+
+                if (regex.IsMatch(input))
+                {
+                    Match match = regex.Match(input);
+
+                    string customer = match.Groups["name"].Value.Substring(1, match.Groups["name"].Value.Length - 2);
+                    string product = match.Groups["product"].Value.Substring(1, match.Groups["product"].Value.Length - 2);
+                    int quantiy = int.Parse(match.Groups["quantity"].Value.Substring(1, match.Groups["quantity"].Value.Length - 2));
+                    double price = double.Parse(match.Groups["price"].Value, culture);
+
+                    totalSum += quantiy * price;
+
+                    Console.WriteLine($"{customer}: {product} - {quantiy * price:F2}");
+                }
+            }
+            Console.WriteLine($"Total income: {totalSum:F2}");
+        }
+    }
+}
---
+++ Exam_prep_part2/SoftUni Exam Results/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni_Exam_Results
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userSubmissions = new Dictionary<string, Dictionary<string, int>>();
+            var exams = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split('-');
+
+                if (input[0] == "exam finished")
+                {
+                    break;
+                }
+
+                string participant = input[0];
+
+                if (input[1] != "banned")
+                {
+                    string language = input[1];
+                    int score = int.Parse(input[2]);
+
+                    if (!userSubmissions.ContainsKey(participant))
+                    {
+                        userSubmissions.Add(participant, new Dictionary<string, int>());
+                    }
+
+                    if (!userSubmissions[participant].ContainsKey(language))
+                    {
+                        userSubmissions[participant].Add(language, 0);
+                    }
+
+                    if (userSubmissions[participant][language] < score)
+                    {
+                        userSubmissions[participant][language] = score;
+                    }
+
+                    if (!exams.ContainsKey(language))
+                    {
+                        exams.Add(language, 0);
+                    }
+
+                    exams[language]++;
+                }
+                else
+                {
+                    userSubmissions.Remove(participant);
+                }
+            }
+
+            Console.WriteLine("Results:");
+            foreach (var participant in userSubmissions.OrderByDescending(n => n.Value.Values.Sum()).ThenBy(n => n.Key))
+            {
+                Console.WriteLine($"{participant.Key} | {participant.Value.Values.Sum()}");
+            }
+            Console.WriteLine("Submissions:");
+            foreach (var exam in exams.OrderByDescending(n => n.Value).ThenBy(n => n.Key))
+            {
+                Console.WriteLine($"{exam.Key} - {exam.Value}");
+            }
+        }
+    }
+}
